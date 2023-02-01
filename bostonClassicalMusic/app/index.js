ce7af4a70357f110bd0a8608dc3d1f262f2845c9
Manny Rocha <mmrocha905@gmail.com>
2023-02-01 16:13:07 +0000

    Need to convert xml to html and style

--- bostonClassicalMusic/app/index.js
+++ bostonClassicalMusic/app/index.js
@@ -1,16 +1,47 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const request = require('request');
+const xml2js = require('xml2js');
+const fs = require('fs');
+
 const app = express();
-const port = 3000;
 
-app.get("/", async (req, res) => {
-  const url = 'https://www.classical-scene.com/?feed=gigpress';
-  const response = await axios.get(url);
-  const xmlText = response.data;
-
-  res.send(xmlText);
+app.get('/', (req, res) => {
+  request('https://www.classical-scene.com/?feed=gigpress', (error, response, xml) => {
+    if (!error && response.statusCode === 200) {
+      xml2js.parseString(xml, (err, result) => {
+        if (!err) {
+          const html = convertXMLToHTML(result);
+          fs.writeFileSync('index.html', html);
+          res.sendFile('../pages/index.html');
+        } else {
+          console.error(err);
+          res.sendStatus(500);
+        }
+      });
+    } else {
+      console.error(error);
+      res.sendStatus(500);
+    }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+function convertXMLToHTML(xml) {
+  let html = '<ul>';
+  xml2js.parseString(xml, (err, result) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    result.rss.channel[0].item.forEach((item) => {
+      html += `<li><h3>${item.title[0]}</h3><p>${item.description[0]}</p></li>`;
+    });
+  });
+  html += '</ul>';
+  return html;
+}
+
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
